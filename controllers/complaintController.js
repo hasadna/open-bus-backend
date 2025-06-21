04625541de73a2b64ca67f993ad3d8045272bab2
Noam Gaash <noam.gaash@applitools.com>
2025-06-21 20:23:30 +0300

    fix: allow dependency injection for axios in complaintController

--- controllers/complaintController.js
+++ controllers/complaintController.js
@@ -4,10 +4,10 @@ import { buildXmlFrom } from '../gov_api/template_builder.js';
 
 const URL = 'https://forms.gov.il/globaldata/getsequence/getHtmlForm.aspx?formType=PniotMot%40mot.gov.il';
 
-export async function complaintController(req, res) {
+export async function complaintController(req, res, myAxios = axios) {
 	try {
 		const xml = buildXmlFrom(req.body);
-		const response = await axios.post(URL, xml, {
+		const response = await myAxios.post(URL, xml, {
 			headers: { 'Content-Type': 'application/xml' },
 		});
 		res.status(200).json({ success: true, data: response });
