3363a7207d5e6d1aa1ab08dc4160dc6429d7ea29
aviv <avivabachi@gmail.com>
2025-06-23 20:22:28 +0300

    - Changed the test script in package.json to run Mocha instead of an error message. - Added .prettierignore file to exclude specific files from formatting. - Created .prettierrc file with custom Prettier configuration settings.

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-import express from 'express';
-import cors from 'cors';
-import { createIssue } from './controllers/issueController.js';
-import { complaintController } from './controllers/complaintController.js';
+import express from 'express'
+import cors from 'cors'
+import { createIssue } from './controllers/issueController.js'
+import { complaintController } from './controllers/complaintController.js'
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+const app = express()
+const PORT = process.env.PORT || 3001
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
 app.get('/', (_, res) => {
-	res.send('alive');
-});
+  res.send('alive')
+})
 
-app.post('/create-issue', createIssue);
-app.post('/complaint', complaintController);
+app.post('/create-issue', createIssue)
+app.post('/complaint', complaintController)
 
 app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}`);
-});
+  console.log(`Server is running on port ${PORT}`)
+})
