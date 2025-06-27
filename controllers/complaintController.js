e4d4a6890fb001862eb99b9b8afd18e432299d89
aviv <avivabachi@gmail.com>
2025-06-27 22:39:00 +0300

    feat: implement createIssue function and associated tests for GitHub issue creation refactor: rename complaintController to sendComplaint and update reference number fetching test: enhance tests for complaintController and getReferenceNumber functions chore: update package.json and package-lock.json with type definitions for express and cors

--- controllers/complaintController.js
+++ controllers/complaintController.js
@@ -1,15 +1,15 @@
 import axios from 'axios'
+
 import { buildXmlFrom } from '../gov_api/template_builder.js'
 import { getReferenceNumber } from '../gov_api/get_reference_number.js'
 
 const URL = 'https://forms.gov.il/globaldata/getsequence/getHtmlForm.aspx?formType=PniotMot%40mot.gov.il'
 
-export async function complaintController(req, res, myAxios = axios) {
+export async function sendComplaint(req, res, myAxios = axios) {
   try {
-    req.body.ReferenceNumber = getReferenceNumber()
+    req.body.ReferenceNumber = getReferenceNumber(req.body.debug)
     const xml = buildXmlFrom(req.body)
     if (req.body.debug) {
-      console.log('XML to be sent:', xml)
       return res.status(200).json({ success: true, debug: true, xml })
     }
     const response = await myAxios.post(URL, xml, {
