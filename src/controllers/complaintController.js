7c67af2b757bd0601af3efc3fae2062a7f10e2e5
aviv <avivabachi@gmail.com>
2025-06-28 09:44:22 +0300

    feat: fastify

--- src/controllers/complaintController.js
+++ src/controllers/complaintController.js
@@ -6,22 +6,22 @@ import { getReferenceNumber } from '../utils/get_reference_number.js'
 const URL = 'https://forms.gov.il/globaldata/getsequence/getHtmlForm.aspx?formType=PniotMot%40mot.gov.il'
 
 /**
- * @param {import('express').Request} req
- * @param {import('express').Response} res
+ * @param {import('fastify').FastifyRequest} request
+ * @param {import('fastify').FastifyReply} reply
  */
-export async function sendComplaint(req, res) {
+export async function sendComplaint(request, reply) {
   try {
-    req.body.ReferenceNumber = getReferenceNumber(req.body.debug)
-    const xml = buildXmlFrom(req.body)
-    if (req.body.debug) {
-      return res.status(200).json({ success: true, debug: true, xml })
+    request.body.ReferenceNumber = getReferenceNumber(request.body.debug)
+    const xml = buildXmlFrom(request.body)
+    if (request.body.debug) {
+      return reply.status(200).send({ success: true, debug: true, xml })
     }
     const response = await axios.post(URL, xml, {
       headers: { 'Content-Type': 'application/xml' },
     })
-    res.status(200).json({ success: true, data: response })
+    return reply.status(200).send({ success: true, data: response })
   } catch (error) {
     console.error('Error creating complain:', error)
-    res.status(500).json({ error: 'Internal Server Error' })
+    return reply.status(500).send({ error: 'Internal Server Error' })
   }
 }
