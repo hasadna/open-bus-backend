a19b131fe27ba90b09663378bd6801261bc6a46f
Aviv <AvivAbachi@gmail.com>
2026-01-24 20:37:04 +0200

    Update getReferenceNumber.js

--- src/utils/getReferenceNumber.js
+++ src/utils/getReferenceNumber.js
@@ -4,16 +4,7 @@ import ky from 'ky';
 const URL = 'https://forms.gov.il/globaldata/getsequence/getHtmlForm.aspx?formType=PniotMot%40mot.gov.il';
 
 export async function getReferenceNumber() {
-  const response = await ky.get(URL, {
-    credentials: 'include',
-    headers: {
-      Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9',
-      'Cache-Control': 'no-cache',
-      Pragma: 'no-cache',
-    },
-    timeout: 30000,
-  });
-
+  const response = await ky.get(URL);
   const html = await response.text();
   const dom = load(html);
   const ref = dom('#ReferenceNumber').text().trim();
